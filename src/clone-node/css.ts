6ee21f6bdb3a18ad61a3379727af6a9fe94f298d
FoHoOV <53280503+FoHoOV@users.noreply.github.com>
2026-07-20 23:09:12 +0330

    perf: defer clone styling to batched inlining

--- src/clone-node/css.ts
+++ src/clone-node/css.ts
@@ -1,12 +1,8 @@
 import type { ClonableElement } from './types'
-import type { Options } from '../types'
-import { getStyleProperties } from '../util'
 
 export function cloneCSSText<T extends ClonableElement>(
   nativeNode: T,
   clonedNode: T,
-  options: Options,
-  preserveReplacementStyle: boolean,
 ) {
   const targetStyle = clonedNode.style
   if (!targetStyle) {
@@ -17,16 +13,5 @@ export function cloneCSSText<T extends ClonableElement>(
   if (sourceStyle.cssText) {
     targetStyle.cssText = sourceStyle.cssText
     targetStyle.transformOrigin = sourceStyle.transformOrigin
-    return
-  }
-
-  if (preserveReplacementStyle && nativeNode.tagName !== clonedNode.tagName) {
-    getStyleProperties(options).forEach((name) => {
-      targetStyle.setProperty(
-        name,
-        sourceStyle.getPropertyValue(name),
-        sourceStyle.getPropertyPriority(name),
-      )
-    })
   }
 }
