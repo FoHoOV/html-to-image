3117ca28c5cbe5097c83eae1d492706723cc071f
FoHoOV <53280503+FoHoOV@users.noreply.github.com>
2026-07-21 01:26:56 +0330

    build: consolidate package artifacts under dist

--- rollup.config.js
+++ rollup.config.js
@@ -1,29 +1,44 @@
 import commonjs from '@rollup/plugin-commonjs'
 import { nodeResolve } from '@rollup/plugin-node-resolve'
 import replace from '@rollup/plugin-replace'
-import terser from '@rollup/plugin-terser'
 import typescript from '@rollup/plugin-typescript'
+import terser from '@rollup/plugin-terser'
 import filesize from 'rollup-plugin-filesize'
 
 export default {
   input: './src/index.ts',
   output: [
     {
+      file: 'dist/esm/index.mjs',
+      format: 'es',
+      sourcemap: true,
+    },
+    {
+      exports: 'named',
+      file: 'dist/cjs/index.cjs',
+      format: 'cjs',
+      sourcemap: true,
+    },
+    {
       name: 'htmlToImage',
       format: 'umd',
-      file: 'dist/html-to-image.js',
+      file: 'dist/browser/html-to-image.js',
+      plugins: [terser()],
       sourcemap: true,
     },
   ],
   plugins: [
-    typescript({ declaration: false, module: 'esnext' }),
+    typescript({
+      declaration: false,
+      inlineSources: true,
+      module: 'esnext',
+    }),
     nodeResolve(),
     commonjs(),
     replace({
       preventAssignment: true,
       'process.env.NODE_ENV': JSON.stringify('production'),
     }),
-    terser(),
     filesize(),
   ],
 }
