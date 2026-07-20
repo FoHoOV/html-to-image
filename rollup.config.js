0340ed5dfb85ed70fce45897b4bc9c9de1711d67
FoHoOV <53280503+FoHoOV@users.noreply.github.com>
2026-07-21 01:21:23 +0330

    build: replace private toolchain packages

--- rollup.config.js
+++ rollup.config.js
@@ -1,6 +1,12 @@
-import config from '@bubkoo/rollup-config'
+import commonjs from '@rollup/plugin-commonjs'
+import { nodeResolve } from '@rollup/plugin-node-resolve'
+import replace from '@rollup/plugin-replace'
+import terser from '@rollup/plugin-terser'
+import typescript from '@rollup/plugin-typescript'
+import filesize from 'rollup-plugin-filesize'
 
-export default config({
+export default {
+  input: './src/index.ts',
   output: [
     {
       name: 'htmlToImage',
@@ -9,4 +15,15 @@ export default config({
       sourcemap: true,
     },
   ],
-})
+  plugins: [
+    typescript({ declaration: false, module: 'esnext' }),
+    nodeResolve(),
+    commonjs(),
+    replace({
+      preventAssignment: true,
+      'process.env.NODE_ENV': JSON.stringify('production'),
+    }),
+    terser(),
+    filesize(),
+  ],
+}
