8f2495292370e45b73bee2dfc53a60538f77b414
FoHoOV <53280503+FoHoOV@users.noreply.github.com>
2026-07-21 00:30:00 +0330

    fix: preserve styles on video replacements

--- src/clone-node/video.ts
+++ src/clone-node/video.ts
@@ -1,6 +1,7 @@
 import type { Options } from '../types'
 import { imageToDataUrl } from '../dataurl'
 import { getMimeType } from '../mimes'
+import { setStyleSource } from '../style'
 import { createImage } from '../util'
 
 export async function cloneVideoElement(
@@ -15,10 +16,16 @@ export async function cloneVideoElement(
     canvas.height = video.clientHeight
     context?.drawImage(video, 0, 0, canvas.width, canvas.height)
 
-    return createImage(canvas.toDataURL())
+    return createVideoImage(video, canvas.toDataURL())
   }
 
   const poster = video.poster
   const dataURL = await imageToDataUrl(poster, getMimeType(poster), options)
-  return createImage(dataURL)
+  return createVideoImage(video, dataURL)
+}
+
+async function createVideoImage(video: HTMLVideoElement, dataURL: string) {
+  const image = await createImage(dataURL)
+  setStyleSource(image, video)
+  return image
 }
