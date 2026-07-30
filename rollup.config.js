860eb58c1e69d6f825ba62c567959b4b3ba96f0e
FoHoOV <53280503+FoHoOV@users.noreply.github.com>
2026-07-30 23:46:26 +0330

    chore: remove redundant packages

--- rollup.config.js
+++ rollup.config.js
@@ -1,6 +1,3 @@
-import commonjs from "@rollup/plugin-commonjs";
-import { nodeResolve } from "@rollup/plugin-node-resolve";
-import replace from "@rollup/plugin-replace";
 import typescript from "@rollup/plugin-typescript";
 import terser from "@rollup/plugin-terser";
 import { defineConfig } from "rollup";
@@ -10,22 +7,25 @@ export default defineConfig({
   input: "./src/index.ts",
   output: [
     {
-      file: "dist/esm/index.mjs",
+      dir: "dist",
+      entryFileNames: "esm/index.mjs",
       format: "es",
       generatedCode: "es5",
       sourcemap: true,
     },
     {
+      dir: "dist",
+      entryFileNames: "cjs/index.cjs",
       exports: "named",
-      file: "dist/cjs/index.cjs",
       format: "cjs",
       generatedCode: "es5",
       sourcemap: true,
     },
     {
+      dir: "dist",
+      entryFileNames: "browser/html-to-image.js",
       name: "htmlToImage",
       format: "umd",
-      file: "dist/browser/html-to-image.js",
       generatedCode: "es5",
       plugins: [terser({ ecma: 5 })],
       sourcemap: true,
@@ -33,23 +33,8 @@ export default defineConfig({
   ],
   plugins: [
     typescript({
-      cacheDir: "node_modules/.cache/rollup-plugin-typescript",
-      compilerOptions: {
-        composite: false,
-        declaration: false,
-        emitDeclarationOnly: false,
-        incremental: true,
-        inlineSources: true,
-        outDir: "./dist",
-      },
-      outputToFilesystem: false,
+      outputToFilesystem: true,
       tsconfig: "./tsconfig.lib.json",
-    }),
-    nodeResolve(),
-    commonjs(),
-    replace({
-      preventAssignment: true,
-      "process.env.NODE_ENV": JSON.stringify("production"),
     }),
     filesize(),
   ],
